7c479d00081022b4e3e644a7a69fcd43874be439
Dean Martin <dean.martin@frostaura.net>
2023-02-06 20:46:00 +0200

    feat(Documentation): Adding of demo config for a component.

---
+++ src/FrostAura.Libraries.Components/Presentational/Status/CountdownBar.razor.demo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FrostAura.Libraries.Components.Abstractions;
+
+namespace FrostAura.Libraries.Components.Presentational.Status
+{
+    /// <summary>
+    /// Component to display a decreasing bar after which an event fires.
+    /// </summary>
+    public partial class CountdownBar : BaseComponent<CountdownBar>, IDisposable
+    {
+        /// <summary>
+        /// Lifecycle event.
+        /// </summary>
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+
+            if (!EnableDemoMode) return;
+
+            Infinite = true;
+            Duration = TimeSpan.FromSeconds(15);
+        }
+    }
+}
